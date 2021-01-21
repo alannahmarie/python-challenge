96f28fc8862dd43bcf21d7d4641f926e4013c24d
Alannah Fleet <alannahmarie@Alannahs-MBP.fios-router.home>
2021-01-20 23:14:50 -0500

    Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,23 +12,52 @@ with open(csvpath) as budget_data:
 
     # Define variables that you will use in for looo
     total_months = 0
-    profit_losses = 0
-    average_change = 0 
-    increase = 0
-    decrease = 0 
-    prev_row = 0 
-    next_row = prev_row + 1
+    profit_losses = 0 
+    change = 0 
+
+    average_change = 0
+
+    month = []
+    money = []
+    
     # Create a for loop that will iterate over all rows of data
     for row in csvreader:
 
-        total_months = total_months + 1
-        profit_losses = profit_losses + (int(row[1]))
-       
+        money.append(int(row[1]))
+        month.append(row[0])
 
+        total_months = total_months + 1
+        profit_losses = sum(money)
+    
+        increase = max(money)
+        decrease = min(money)
+
+        max_index = money.index(increase)
+        min_index = money.index(decrease)
+        
+        
+
+    change = money[-1] - money[0]
+    average_change = round((change) / (total_months -1), 2)
+
+
+print(" ")
 print("Financial Valuation")
 print("----------------------------------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${profit_losses}")
-print(f"Average Change: {average_change}")
-print(f"Greatest Increase in Profits: $ {increase}")
-print(f"Greatest Decrease in Profits: $")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {month[max_index]} (${money[max_index]})")
+print(f"Greatest Decrease in Profits: {month[min_index]} (${money[min_index]})")
+
+output_path = "/Users/alannahmarie/Desktop/python-challenge/PyBank/Analysis/analysis.txt"
+
+with open(output_path, 'w', newline='') as txt:
+    txt.write(" \n")
+    txt.write('Financial Valuation\n') 
+    txt.write('----------------------------------------------------\n') 
+    txt.write(f"Total Months: {total_months}\n") 
+    txt.write(f"Total: ${profit_losses}") 
+    txt.write(f"Average Change: ${average_change}\n") 
+    txt.write(f"Greatest Increase in Profits: {month[max_index]} (${money[max_index]})\n") 
+    txt.write(f"Greatest Decrease in Profits: {month[min_index]} (${money[min_index]})") 
